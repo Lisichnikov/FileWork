13afd74218cdafab2a15a677dac0130fb6fa4a43
Алексей Лисичников <alexochanel@gmail.com>
2023-03-15 17:40:23 +0300

    леее што за бархатна 11 лаба кефтеме

--- FileWork/Program.cs
+++ FileWork/Program.cs
@@ -7,12 +7,25 @@ namespace FileWork
     {
         static void Main(string[] args)
         {
-            StreamReader text = new StreamReader(@"C:\Users\alexo\OneDrive\Рабочий стол\fileex\file.txt");
-            string[] array = text.ReadToEnd().Split('\n');
-            foreach (var i in array)
+            try
+            {
+
+                StreamReader text = new StreamReader(@"C:\Users\alexo\OneDrive\Рабочий стол\fileex\file.txt");
+                string[] array = text.ReadToEnd().Split('\n');
+                foreach (var i in array)
             {
                 Console.WriteLine(i);
             }
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
