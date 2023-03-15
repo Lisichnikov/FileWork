2cdeb31dd30d62746bdb5c5801f6faf219cb5d66
Алексей Лисичников <alexochanel@gmail.com>
2023-03-15 17:28:26 +0300

    Добавьте файлы проекта.

---
+++ FileWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader text = new StreamReader(@"C:\Users\alexo\OneDrive\Рабочий стол\fileex\file.txt");
+            string[] array = text.ReadToEnd().Split('\n');
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
